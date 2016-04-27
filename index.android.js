8201f128afb348a43f5f9c56f9443f35faf9470a
Adrian Castro <Adrian Castro>
2016-04-26 23:43:56 -0500

    Initial mockup to chat

--- index.android.js
+++ index.android.js
@@ -8,22 +8,112 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  ScrollView,
+  Image,
+  TextInput,
 } from 'react-native';
 
 class reactChat extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+        <ScrollView style={styles.chatContainer}>
+          <View style={styles.messages}>
+            <Text>message</Text>
+            <Image
+              style={styles.iconMsg}
+              source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+            />
+          </View>
+          <View style={styles.messages}>
+            <Text>message</Text>
+            <Image
+              style={styles.iconMsg}
+              source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+            />
+          </View>  
+          <View style={styles.messages}>
+            <Text>message</Text>
+            <Image
+              style={styles.iconMsg}
+              source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+            />
+          </View>
+          <View style={styles.messages}>
+            <Text>message</Text>
+            <Image
+              style={styles.iconMsg}
+              source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+            />
+          </View>
+          <View style={styles.messages}>
+            <Text>message</Text>
+            <Image
+              style={styles.iconMsg}
+              source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+            />
+          </View>  
+          <View style={styles.messages}>
+            <Text>message</Text>
+            <Image
+              style={styles.iconMsg}
+              source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+            />
+          </View><View style={styles.messages}>
+            <Text>message</Text>
+            <Image
+              style={styles.iconMsg}
+              source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+            />
+          </View>
+          <View style={styles.messages}>
+            <Text>message</Text>
+            <Image
+              style={styles.iconMsg}
+              source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+            />
+          </View>  
+          <View style={styles.messages}>
+            <Text>message</Text>
+            <Image
+              style={styles.iconMsg}
+              source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+            />
+          </View><View style={styles.messages}>
+            <Text>message</Text>
+            <Image
+              style={styles.iconMsg}
+              source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+            />
+          </View>
+          <View style={styles.messages}>
+            <Text>message</Text>
+            <Image
+              style={styles.iconMsg}
+              source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+            />
+          </View>  
+          <View style={styles.messages}>
+            <Text>message</Text>
+            <Image
+              style={styles.iconMsg}
+              source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+            />
+          </View>
+        </ScrollView>
+        <View style={styles.messageText}>
+          <Text style={styles.inputMessage}>
+            hola
+          </Text>
+          <Image
+            style={styles.icon}
+            source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+          />
+        </View>
+         <View>
+          <TextInput style={styles.inputMessage} placeholder={'Please choose an username'} multiline={true} />
+        </View>
       </View>
     );
   }
@@ -36,15 +126,39 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  chatContainer: {
+    flex: 5,
+    alignSelf: 'stretch',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderStyle:'dashed',
+    padding: 5,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  messageText: {
+    flex:1,
+    flexDirection:'row',
+    margin: 20,
+  },
+  messages: {
+    flex:1,
+    flexDirection:'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 20,
+  },  
+  icon: {
+    width: 100,
+    height: 100,
+  },
+  iconMsg: {
+    width: 40,
+    height: 40,
+  },
+  inputMessage: {
+    borderWidth: 1,
+    borderRadius: 5,
+    borderStyle:'solid',
+    padding: 5,
   },
 });
 
